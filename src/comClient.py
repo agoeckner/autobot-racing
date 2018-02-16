763656ec6988de0e8431052424dfcffacd3eead9
Ben Huemann <benhuemann@gmail.com>
2018-02-16 17:48:53 -0500

    Further work on communication files.

--- src/comClient.py
+++ src/comClient.py
@@ -7,6 +7,7 @@ class Car:
 	name = None
 	ip = None
 	port = None
+	connection = None
 	isConnected = False
 
 	def __init__(self, name, ip, port):
@@ -19,23 +20,35 @@ class Car:
 		return (name, ip, port)
 
 	#set attributes in tuple form (name, ip, port). 
-	#None can be set for any attribute to skip attr assignment
+	#None can be set for any attribute to skip assignment
 	def setAttr(attr):
 		#Todo add handling if already connected!
 		if attr[0]: name = attr[0]
 		if attr[1]: ip = attr[1]
 		if attr[2]: port = attr[2]
-		
+	
+	#Simply send strings. This will change when we decide on needs to be sent.
+	def sendMsg(str):
+		connection.sendall(str.encode('ascii'))
+	
+	def getMsg():
+		return connection.recv(1024).decode('ascii')
+	
 	def connectToHost():
 			
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-		printf('Connecting to %s..." % ip)
-		s.connect((ip, port))
-		printf('Connected!")
-
+		try:
+			connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			
+			#Will raise a socket.timeout exception after 5 seconds if connection is not made
+			s.create_connection((ip, port), 5)
+			
+		except socket.timeout:
+			raise TimeoutError
+		
 		isConnected = True
+
 		
 	def disconnectFromHost():
-		s.close()
+		connection.close()
+		connection = None
 		isConnected = False
--- src/comServer.py
+++ src/comServer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python           # This is server.py file
 
 import socket
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -8,6 +9,8 @@ hostname = socket.gethostbyname()
 ip = socket.gethostbyname(hostname)
 port = 5000
 
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setblocking(False)
 s.bind((ip, port))
 s.listen()
 
@@ -19,5 +22,8 @@ print('Connection established to: ', con[0])
 
 while True:
         #add car event loop here
-        pass
-    
+		#for now print what is received
+		
+		msg = s.recv(1024)
+		print(msg.decode('ascii'))
+		time.sleep(2);
