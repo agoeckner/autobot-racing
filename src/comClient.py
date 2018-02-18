b70363292884081d83befb39a838501d4126cae4
Ben Huemann <benhuemann@gmail.com>
2018-02-18 18:58:55 -0500

    Fixed some issues preventing connection

--- src/comClient.py
+++ src/comClient.py
@@ -16,31 +16,31 @@ class Car:
 		self.port = port
 		
 	#gets attributes in tuple form (name, ip, port)
-	def getAttr()
+	def getAttr(self, ):
 		return (name, ip, port)
 
 	#set attributes in tuple form (name, ip, port). 
 	#None can be set for any attribute to skip assignment
-	def setAttr(attr):
+	def setAttr(self, attr):
 		#Todo add handling if already connected!
 		if attr[0]: name = attr[0]
 		if attr[1]: ip = attr[1]
 		if attr[2]: port = attr[2]
 	
 	#Simply send strings. This will change when we decide on needs to be sent.
-	def sendMsg(str):
+	def sendMsg(self, str):
 		connection.sendall(str.encode('ascii'))
 	
-	def getMsg():
+	def getMsg(self):
 		return connection.recv(1024).decode('ascii')
 	
-	def connectToHost():
+	def connectToHost(self):
 			
 		try:
-			connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			#connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			
 			#Will raise a socket.timeout exception after 5 seconds if connection is not made
-			s.create_connection((ip, port), 5)
+			connection = socket.create_connection((self.ip, self.port), 5)
 			
 		except socket.timeout:
 			raise TimeoutError
@@ -48,7 +48,7 @@ class Car:
 		isConnected = True
 
 		
-	def disconnectFromHost():
+	def disconnectFromHost(self):
 		connection.close()
 		connection = None
 		isConnected = False
--- src/comServer.py
+++ src/comServer.py
@@ -5,19 +5,19 @@ import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-hostname = socket.gethostbyname()
-ip = socket.gethostbyname(hostname)
+hostname = socket.gethostname()
+ip = "192.168.2.3" #socket.gethostbyname(hostname)
 port = 5000
 
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.setblocking(False)
 s.bind((ip, port))
 s.listen()
 
-print('Server started!')
+print('Server started! IP="%s" PORT="%s"' %(ip, port))
 print('Waiting for connection...')
 
 con = s.accept()
+#s.setblocking(False)
 print('Connection established to: ', con[0])
 
 while True:
@@ -26,4 +26,5 @@ while True:
 		
 		msg = s.recv(1024)
 		print(msg.decode('ascii'))
+		s.send("Ack".encode("ascii"))
 		time.sleep(2);
