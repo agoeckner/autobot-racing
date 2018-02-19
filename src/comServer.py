c35ecbfc4319d542e263e7e390e814f9d0d87208
Ben Huemann <benhuemann@gmail.com>
2018-02-19 18:53:03 -0500

    Fixed issues preventing connection

--- src/comServer.py
+++ src/comServer.py
@@ -3,28 +3,27 @@
 import socket
 import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-hostname = socket.gethostname()
-ip = "192.168.2.3" #socket.gethostbyname(hostname)
+ip = '192.168.2.4'
 port = 5000
 
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((ip, port))
-s.listen()
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serverSocket.bind((ip, port))
+serverSocket.listen(1)
 
 print('Server started! IP="%s" PORT="%s"' %(ip, port))
 print('Waiting for connection...')
 
-con = s.accept()
-#s.setblocking(False)
-print('Connection established to: ', con[0])
+clientSocket, addr = serverSocket.accept()
+print('Connection established to: ', addr)
+
 
 while True:
         #add car event loop here
 		#for now print what is received
 		
-		msg = s.recv(1024)
+		msg = clientSocket.recv(1024)
 		print(msg.decode('ascii'))
-		s.send("Ack".encode("ascii"))
+		clientSocket.send("Ack".encode("ascii"))
 		time.sleep(2);
