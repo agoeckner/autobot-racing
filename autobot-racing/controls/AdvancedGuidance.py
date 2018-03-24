c32d0070c9c906f7be6411e9f1b908dbc4b5cf17
Anthony Goeckner <agoeckne@purdue.edu>
2018-03-24 16:20:51 -0400

    Added basic passing system. Vehicle increases distance from wall when approaching another vehicle.

--- autobot-racing/controls/AdvancedGuidance.py
+++ autobot-racing/controls/AdvancedGuidance.py
@@ -4,16 +4,47 @@ from math import *
 from Utilities import *
 from .Controls import *
 
-class PassingGuidanceSystem(WallFollowingGuidanceSystem):	
+class PassingGuidanceSystem(WallFollowingGuidanceSystem):
+	FOLLOW_PASS_THRESHOLD_DIST = 40
+	MIN_PASS_SPACING = 15
+	
 	def __init__(self, *args, **kwargs):
 		super(PassingGuidanceSystem, self).__init__(*args, **kwargs)
+		self.origWallDist = self.desiredWallDist
 	
 	# Returns the desired heading at a specific position on the track.
 	def getDesiredHeading(self, pos):
+		# Get distance from wall.
 		((wall0, wall1), d) = self._getClosestPolyVertex(pos, self.track.innerWall)
+		self.actualWallDist = d
+		
+		# Attempt to pass if necessary.
+		wallDist = self.getPassingWallDist(self.vehicle)
+		self.desiredWallDist = wallDist
+		
 		# Straight-line heading along wall.
 		ha = atan2(wall1[1] - wall0[1], wall1[0] - wall0[0])
+		
 		# Heading that converges to correct distance from wall.
-		hd = atan2(d - self.distWall, self.distLookahead)
+		hd = atan2(d - wallDist, self.distLookahead)
 		heading = ha - hd
+		
 		return heading
+	
+	# Determines how much the wall distance needs to change in order to pass.
+	def getPassingWallDist(self, vehicle):
+		vehicles = self.environment.vehicles
+		for v in vehicles:
+			if v == vehicle:
+				continue
+			d = euclideanDistance(vehicle.position, v.position)
+			if d > self.FOLLOW_PASS_THRESHOLD_DIST:
+				continue
+			if v.speed >= vehicle.speed:
+				continue
+			# print("PASSING")
+			if abs(v.guidance.actualWallDist - self.desiredWallDist) >= self.MIN_PASS_SPACING:
+				return self.desiredWallDist
+			return v.guidance.actualWallDist + self.MIN_PASS_SPACING
+		# print("NOT PASSING")
+		return self.origWallDist
