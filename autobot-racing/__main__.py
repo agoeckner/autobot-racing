b9ffbfc40f12daff5c24b6c2f8007b5d771d6a9e
Harold <smithhe@purdue.edu>
2018-03-27 19:02:47 -0400

    Started implementing a queue for the camera feed

--- autobot-racing/__main__.py
+++ autobot-racing/__main__.py
@@ -1,94 +1,99 @@
 from EthernetInterface import EthernetInterface
 from UIManager import UIManager
 from cv.ComputerVision import *
+import _thread
+import threading
 
 class FrameworkManager(): #{
-	##-----------------------------------------------------------------------------
-	## Constructor
-	##-----------------------------------------------------------------------------
-	def __init__(self): #{
-		self.cv = ComputerVision()
-		self.UserInterface = UIManager(self)
-		self.EthernetInterface = EthernetInterface(self)
-		self.carList = [] #Stores all car objects
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Constructor
+        ##-----------------------------------------------------------------------------
+        def __init__(self): #{
+                self.cv = ComputerVision()
+                self.UserInterface = UIManager(self)
+                self.EthernetInterface = EthernetInterface(self)
+                self.carList = [] #Stores all car objects
+        #}
 
-	##-----------------------------------------------------------------------------
-	## Opens the User Interface
-	##-----------------------------------------------------------------------------
-	def startup(self): #{
-		self.UserInterface.openCarStatsUI()
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Opens the User Interface
+        ##-----------------------------------------------------------------------------
+        def startup(self): #{
+                tUI = threading.Thread(target=self.UserInterface.openCarStatsUI)
+                tCV = threading.Thread(target=self.cv.run, args=(True,))
+                tUI.start()
+                tCV.start()
+        #}
 
 ##Car Methods-----------------------------------------------------------------------------------------------------------------------------------------
-	##-----------------------------------------------------------------------------
-	## Returns the current list of cars
-	##-----------------------------------------------------------------------------
-	def getCarList(self): #{
-		return self.carList
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Returns the current list of cars
+        ##-----------------------------------------------------------------------------
+        def getCarList(self): #{
+                return self.carList
+        #}
 
-	##-----------------------------------------------------------------------------
-	## Updates the list of cars
-	##-----------------------------------------------------------------------------
-	def updateCarList(self, newList): #{
-		self.carList = newList
-		print(len(self.carList))
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Updates the list of cars
+        ##-----------------------------------------------------------------------------
+        def updateCarList(self, newList): #{
+                self.carList = newList
+                print(len(self.carList))
+        #}
 ##----------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 ##UI Methods------------------------------------------------------------------------------------------------------------------------------------------
-	##-----------------------------------------------------------------------------
-	## Updates the list of cars
-	##-----------------------------------------------------------------------------
-	def updateCarFrameColor(self, carName, status): #{
-		self.UserInterface.changeCarFrameColor(carName, status)
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Updates the list of cars
+        ##-----------------------------------------------------------------------------
+        def updateCarFrameColor(self, carName, status): #{
+                self.UserInterface.changeCarFrameColor(carName, status)
+        #}
 
-	##-----------------------------------------------------------------------------
-	## Gets the a list of Control Systems
-	##-----------------------------------------------------------------------------
-	def getControlSystems(self): #{
-		return ['Option 1', 'Option 2']
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Gets the a list of Control Systems
+        ##-----------------------------------------------------------------------------
+        def getControlSystems(self): #{
+                return ['Option 1', 'Option 2']
+        #}
 
-	##-----------------------------------------------------------------------------
-	## Gets the a list of Guidance Systems
-	##-----------------------------------------------------------------------------
-	def getGuidanceSystems(self): #{
-		return ['Option 1', 'Option 2']
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Gets the a list of Guidance Systems
+        ##-----------------------------------------------------------------------------
+        def getGuidanceSystems(self): #{
+                return ['Option 1', 'Option 2']
+        #}
 
 ##----------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
 ##EthernetInterface Functions-------------------------------------------------------------------------------------------------------------------------
-	##-----------------------------------------------------------------------------
-	## Connects to the PI of the newly added car
-	##-----------------------------------------------------------------------------
-	def connectNewCar(self, car): #{
-		self.EthernetInterface.connectToPI(car)
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Connects to the PI of the newly added car
+        ##-----------------------------------------------------------------------------
+        def connectNewCar(self, car): #{
+                self.EthernetInterface.connectToPI(car)
+        #}
 
-	##-----------------------------------------------------------------------------
-	## Updates the connection of the specified car
-	##-----------------------------------------------------------------------------
-	def updateCarConnection(self, car): #{
-		self.EthernetInterface.updateConnection(car)
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Updates the connection of the specified car
+        ##-----------------------------------------------------------------------------
+        def updateCarConnection(self, car): #{
+                self.EthernetInterface.updateConnection(car)
+        #}
 
-	##-----------------------------------------------------------------------------
-	## Disconnects from the specified car
-	##-----------------------------------------------------------------------------
-	def removeConnection(self, car): #{
-		self.EthernetInterface.disconnectFromPI(car)
-	#}
+        ##-----------------------------------------------------------------------------
+        ## Disconnects from the specified car
+        ##-----------------------------------------------------------------------------
+        def removeConnection(self, car): #{
+                self.EthernetInterface.disconnectFromPI(car)
+        #}
 ##----------------------------------------------------------------------------------------------------------------------------------------------------
 
 #}
 
 if __name__ == "__main__":
-	manager = FrameworkManager()
-	manager.startup()
+        manager = FrameworkManager()
+        manager.startup()
