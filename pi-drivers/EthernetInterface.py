2035b33fb81166d0e8a9a255fd95369d6c4ee6be
Ben Huemann <benhuemann@gmail.com>
2018-03-30 01:11:42 -0400

    API integration work

---
+++ pi-drivers/EthernetInterface.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+import socket
+import struct
+from subprocess import check_output as call
+import RCDriver
+
+class EthernetInterface:
+
+	ip = None
+	port =  None
+	addr = None
+	serverSocket = None
+	clientSocket = None
+	unpacker = None
+	
+	def __init__(self):
+		
+		#messages should be formated (2 byte short, 4 byte float)
+		self.unpacker = struct.Struct('=h h')
+		
+		#Grab LAN ip address. 
+		#Note: we cannot use socket related functions as they return a loopback address		
+		self.ip = call(['hostname', '-I'])
+		self.port = 4000
+		self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		
+		self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.serverSocket.bind((self.ip, self.port))
+		self.serverSocket.listen(0)
+		
+
+	def connectToClient(self):
+		self.clientSocket, self.addr = self.serverSocket.accept()
+
+	def disconnectFromClient(self):
+		self.clientSocket.close()
+		self.clientSocket = None
+		
+	def sendMsg(self, str):
+		try:
+			self.clientSocket.sendall(str.encode('ascii'))
+		except Exception:
+			self.clientSocket = None
+			raise
+	
+	def getMsg(self):
+		
+		try:
+		
+			bin = self.clientSocket.recv(self.unpacker.size)
+			
+			if len(bin) != self.unpacker.size:
+				raise ConnectionError("Unexpected bytes from pipe: expected %i, received %i" % (self.unpacker.size, len(bin)))
+				
+			data = self.unpacker.unpack(bin)		
+			return data
+			
+		except Exception:
+			self.clientSocket = None
+			raise
+			
+
+			
+#Cars event loop
+#All Exception handling to be done here
+try:
+	interface = EthernetInterface()
+	interface.connectToClient()
+	RCDriver.init()
+	
+	print("Starting Event Loop")
+	while True:
+			data = interface.getMsg()
+			RCDriver.setDirection(data[0])
+			RCDriver.setSpeed(data[1])
+
+		
+except Exception:
+		pass
+
+finally:
+		RCDriver.deinit()
+
+
