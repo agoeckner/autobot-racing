5857c284bc4355748fd984f0eca823c61b1f44c7
Anthony Goeckner <agoeckne@purdue.edu>
2018-04-11 15:13:34 -0400

    Updated CV parameters to allow for stable object recognition.

--- autobot-racing/cv/ComputerVision.py
+++ autobot-racing/cv/ComputerVision.py
@@ -13,25 +13,29 @@ class ComputerVision:
 		# (255, 0, 255),
 	)
 	
+	# Used to determine the cutoff for black/white
+	WHITE_THRESHOLD = 130
+	
 	# Used as a tuning constant for the epsilon value of approxPolyDP.
-	APPROX_ARCLEN_MULTIPLIER = 0.03
+	APPROX_ARCLEN_MULTIPLIER = 0.11
 	
 	# The boundary sizes (in pixels squared) of contours that are acceptable
 	# for further processing.
-	CONTOUR_AREA_MIN = 30
-	CONTOUR_AREA_MAX = 20000
+	CONTOUR_AREA_MIN = 100
+	CONTOUR_AREA_MAX = 200
 	
 	# The actual, measured ratio of the triangle targets placed on the vehicles.
 	TRIANGLE_RATIO = 0.48507125007 #0.363636
 	
 	# The tolerance used to check if triangles conform to TRIANGLE_RATIO.
-	TRIANGLE_RATIO_TOLERANCE = 0.06
+	TRIANGLE_RATIO_TOLERANCE = 0.1
 	
 	
 	# Initializes the computer vision system. Set videoDevice to 0 for camera, -1 for Kinect.
 	def __init__(self, parent, videoDevice = 0):
 		self.parent = parent
-		self.queue = parent.telemetryQueue
+		if parent != None:
+			self.queue = parent.telemetryQueue
 	
 		# Run in Kinect mode.
 		if videoDevice == -1:
@@ -52,13 +56,16 @@ class ComputerVision:
 			ret, frame = self.cap.read()
 			if not ret:
 				raise CameraException("Unable to read from video device.")
+		# self.out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
 	
 	# Closes all devices, etc. This function should be called when done with CV.
 	def close(self):
+		print("CLOSE COMPUTER VISION STREAM")
 		if self.mode == "KINECT":
 			self.kinect.close()
 		else:
 			self.cap.release()
+		self.out.release()
 		cv2.destroyAllWindows()
 
 	# The primary computer vision system loop. This polls the camera as fast as
@@ -93,16 +100,15 @@ class ComputerVision:
 	def _processKinectFrame(self, frame):
 		video = np.empty((480,640,4),np.uint8)
 		frame.image.copy_bits(video.ctypes.data)
+		# self.out.write(video)
 		self.processFrame(video)
 	
-	def processFrame(self, frame, showFrame = False, draw = True):
+	def processFrame(self, frame, showFrame = True, draw = True):
 		# Preprocess the image.
-		# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		# ret,thresh = cv2.threshold(gray,120,255,0)
 		blurred = cv2.GaussianBlur(frame, (5, 5), 0)
 		gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-		lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
-		thresh = cv2.threshold(gray, 160, 255, cv2.THRESH_BINARY)[1]
+		# lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
+		thresh = cv2.threshold(gray, self.WHITE_THRESHOLD, 255, cv2.THRESH_BINARY)[1]
 		
 		# Find all contours.
 		im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,
@@ -120,6 +126,7 @@ class ComputerVision:
 			# Check proportions of the triangle.
 			if not self.checkTriangleProportions(approx):
 				continue
+			# cv2.drawContours(frame, [c], -1, (0,255,0), 2)
 			
 			# Get the position of the triangle center.
 			M = cv2.moments(approx)
@@ -132,23 +139,30 @@ class ComputerVision:
 
 			# Determine color of the pixel at the center of the contour.
 			pixel = frame[cY][cX]
-			color = (int(pixel[0]), int(pixel[1]), int(pixel[2]))
-			color = self.getClosestColor(color)
+			
+			# This is the color in OpenCV format, BGR
+			colorCV = (int(pixel[0]), int(pixel[1]), int(pixel[2]))
+			colorCV = self.getClosestColor(colorCV)
+			
+			# This is the color in normal format, RGB.
+			color = (colorCV[2], colorCV[1], colorCV[0])
 			
 			# Push the data out to the main thread.
-			if self.queue != None:
+			if self.parent != None:
 				self.queue.put((center, heading, color))
 		 
 			# Draw the contour and center of the shape on the image.
 			if draw:
-				cv2.drawContours(frame, [approx], -1, color, 3)
+				cv2.drawContours(frame, [approx], -1, colorCV, 3)
 				cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
 				label = "({:d}, {:d}), {:.2f}".format(cX, cY, math.degrees(heading))
+				# label = str(cv2.contourArea(c))
 				cv2.putText(frame, label, (cX - 20, cY - 20),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 		
 		#Put the frame in the queue for the UI
-		self.parent.UIQueue.q.put(('CamFeed', frame))
+		if self.parent != None:
+			self.parent.UIQueue.q.put(('CamFeed', frame))
 
 		if showFrame:
 			# display = np.dstack((frame, gray))
@@ -254,4 +268,9 @@ class CameraException(Exception):
 
 if __name__ == "__main__":
 	# Debug mode
-	ComputerVision(None).run(showFrame=True)
+	cv = ComputerVision(None, "output.avi")
+	try:
+		cv.run(showFrame=True)
+	except KeyboardInterrupt as e:
+		cv.close()
+		raise e
