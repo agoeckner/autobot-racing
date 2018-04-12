e2c4c90643c9d167bf45c3db55c25775f139c359
Anthony Goeckner <agoeckne@purdue.edu>
2018-04-11 22:22:27 -0400

    Fix line accidentally deleted during merge.

--- autobot-racing/__main__.py
+++ autobot-racing/__main__.py
@@ -25,7 +25,7 @@ class FrameworkManager():
 		self.UIQueue = MessageQueue(self)
 
 		# Set up components.
-		self.cv = ComputerVision(self, 0)
+		self.cv = ComputerVision(self, -1)
 		self.UserInterface = UIManager(self)
 		# self.EthernetInterface = EthernetInterface(self)
 		self.carList = [] #Stores all car objects
@@ -58,6 +58,7 @@ class FrameworkManager():
 	def run(self):
 		tUI = threading.Thread(target=self.UserInterface.openCarStatsUI)
 		tCV = threading.Thread(target=self.cv.run, args=(False,))
+		tQueue = threading.Thread(target=self.UIQueue.workerUI)
 		tCV.daemon = True
 		tUI.daemon = True
 		tQueue.daemon = True
